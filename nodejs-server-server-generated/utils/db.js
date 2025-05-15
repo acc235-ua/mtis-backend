f6a2ed0a7727086af211c26cb0d861c1062ff5e0
Pablo Sánchez Pérez <psp59@alu.ua.es>
2025-05-15 13:54:37 +0200

    Restructuración en en la estructura de la db e implementación de la conexion con db

---
+++ nodejs-server-server-generated/utils/db.js
@@ -0,0 +1,22 @@
+// utils/db.js
+'use strict';
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',       // o la IP del servidor MySQL
+  user: 'root',
+  password: 'robinella7', //Cambiar contraseña
+  port: 3307,             // Puerto por defecto de MySQL
+  database: 'mtis-grupal'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error conectando a MySQL:', err.stack);
+    return;
+  }
+  console.log('Conectado a MySQL como ID ' + connection.threadId);
+});
+
+module.exports = connection;
