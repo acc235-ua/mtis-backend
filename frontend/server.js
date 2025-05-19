021b85eb966f086183154998b8960811df03ed36
acc235-ua <acc235@alu.ua.es>
2025-05-19 11:48:04 +0200

    cambios generales

---
+++ frontend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.post('/enviar-soap', async (req, res) => {
+  try {
+    const { soapBody } = req.body;
+    const { data } = await axios.post('http://localhost:9091/empleados', soapBody, {
+      headers: {
+        'Content-Type': 'text/xml; charset=utf-8'
+      }
+    });
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err.toString());
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor activo en http://localhost:${PORT}`);
+});
+
+
+
+
+app.post('/solicita-perito', async (req, res) => {
+  try {
+    const dni = req.query.dni;
+    const { data } = await axios.post(`http://localhost:8081/solicitaperito?dni=${dni}`);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send('Error al solicitar perito: ' + err.message);
+  }
+});
+
